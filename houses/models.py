b37bf17e82a38da868d9df816b4ae77c55d00c05
조정훈 <jeongh1021@naver.com>
2023-02-02 12:50:28 +0900

    장고에서 기본으로 제공하는 user앱을 custom user앱으로 전환(장고에서 프로젝트 시작 할 때마다 바로 하는걸로 추천함)

--- houses/models.py
+++ houses/models.py
@@ -1,16 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
 class House(models.Model):
 
-    """House Model Definition"""
+    """Model Definition for Houses"""
 
-    name = models.CharField(max_length=140)
-    price_per_night = models.PositiveIntegerField(verbose_name="price",help_text="Price per night")
+    name = models.CharField(
+        max_length=140,
+    )
+    price_per_night = models.PositiveIntegerField(
+        verbose_name="Price",
+        help_text="Positive Numbers Only",
+    )
     description = models.TextField()
-    address = models.CharField(max_length=140)
-    pets_allowed = models.BooleanField(default=True ,help_text="Are pets allowed?")
-
+    address = models.CharField(
+        max_length=140,
+    )
+    pets_allowed = models.BooleanField(
+        verbose_name="Pets Allowed?",
+        default=True,
+        help_text="Does this house allow pets?",
+    )
+    owner = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.name
