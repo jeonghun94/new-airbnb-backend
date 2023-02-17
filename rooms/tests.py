51216b2f699062adeb5971d00d1c15dbcca21040
조정훈 <jeongh1021@naver.com>
2023-02-17 18:46:24 +0900

    test 파일 첫 작성 및 테스트

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
 # Create your tests here.
+
+
+class test_cal(APITestCase):
+    def test_two_plus_two(self):
+        self.assertEqual(2 + 1, 4, "2+2 should be 4")
